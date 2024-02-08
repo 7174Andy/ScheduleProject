a0d84d3c1ec8d69b8af986db81059c601d48e78a
7174Andy <hep003@ucsd.edu>
2024-02-07 21:05:21 -0800

    Add design

--- ScheduleApp/app/config/colors.js
+++ ScheduleApp/app/config/colors.js
@@ -1,3 +1,7 @@
 export default {
   backgroundColor: "#292929",
+  textColor: "#fff",
+  black: "#000",
+  tagColor: "#9C9898",
+  marginToProfile: 20,
 };
--- ScheduleApp/app/screens/ProfileScreen.js
+++ ScheduleApp/app/screens/ProfileScreen.js
@@ -1,5 +1,12 @@
-import { SafeAreaView, StyleSheet, Text, View, Image } from "react-native";
-import React from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  Pressable,
+} from "react-native";
 
 import colors from "../config/colors";
 
@@ -7,11 +14,37 @@ export default function ProfileScreen() {
   return (
     <View style={styles.background}>
       <SafeAreaView>
-        <View>
+        <View style={styles.profileContainer}>
           <Image
             style={styles.profileImage}
             source={require("../assets/user.png")}
           />
+          <View style={{ alignItems: "flex-start", flexDirection: "column" }}>
+            <Text style={styles.profileName}>Nathan Chang</Text>
+            <Text style={styles.profileTag}>@Jiwoong</Text>
+            <View style={{ flexDirection: "row", justifyContent: "center" }}>
+              <Pressable
+                style={styles.myCalendarBtn}
+                onPress={() => console.log("My Calendar")}
+              >
+                <Text
+                  style={{ fontSize: 15, padding: 7, color: colors.textColor }}
+                >
+                  My Calendar
+                </Text>
+              </Pressable>
+              <Pressable
+                style={styles.myCalendarBtn}
+                onPress={() => console.log("Vibeee")}
+              >
+                <Text
+                  style={{ fontSize: 15, padding: 7, color: colors.textColor }}
+                >
+                  Vibe
+                </Text>
+              </Pressable>
+            </View>
+          </View>
         </View>
       </SafeAreaView>
     </View>
@@ -23,8 +56,34 @@ const styles = StyleSheet.create({
     backgroundColor: colors.backgroundColor,
     flex: 1,
   },
+  myCalendarBtn: {
+    marginTop: 10,
+    marginLeft: 15,
+    backgroundColor: colors.tagColor,
+    borderRadius: 6,
+  },
+  profileContainer: {
+    alignItems: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: colors.marginToProfile,
+  },
   profileImage: {
-    height: 100,
-    width: 100,
+    height: 115,
+    width: 115,
+    marginRight: 10,
+  },
+  profileName: {
+    color: colors.textColor,
+    fontSize: 23,
+    fontWeight: "bold",
+    marginLeft: colors.marginToProfile,
+  },
+  profileTag: {
+    fontSize: 15,
+    marginLeft: colors.marginToProfile,
+    marginTop: 10,
+    color: colors.tagColor,
   },
 });
