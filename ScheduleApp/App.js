c1f10fa59104825364f7c9cf75828ac93fa63d8f
7174Andy <hep003@ucsd.edu>
2024-03-06 21:00:14 -0800

    Added Navigator

--- ScheduleApp/App.js
+++ ScheduleApp/App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import ProfileScreen from "./app/screens/ProfileScreen";
+import MainContainer from "./app/navigator/MainContainer";
 
 export default function App() {
-  return <ProfileScreen />;
+  return <MainContainer />;
 }
 
 const styles = StyleSheet.create({});
---
+++ ScheduleApp/app/navigator/MainContainer.js
@@ -0,0 +1,37 @@
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  Pressable,
+  ScrollView,
+} from "react-native";
+import React from "react";
+import colors from "../config/colors";
+
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+
+import ProfileScreen from "../screens/ProfileScreen";
+import FriendsScreen from "../screens/FriendsScreen";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+// Tab Names
+const homeName = "Home";
+const friendName = "Friends";
+const profileName = "Profile";
+
+const Tab = createBottomTabNavigator();
+
+export default function MainContainer() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Friends" component={FriendsScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
