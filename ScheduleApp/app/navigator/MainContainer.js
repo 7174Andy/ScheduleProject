77f0c372057f5a2b561928a208d8a06bbbc13ba7
7174Andy <hep003@ucsd.edu>
2024-03-06 21:17:53 -0800

    designing nav bar

--- ScheduleApp/app/navigator/MainContainer.js
+++ ScheduleApp/app/navigator/MainContainer.js
@@ -25,10 +25,17 @@ const profileName = "Profile";
 
 const Tab = createBottomTabNavigator();
 
+const globalScreenOptions = {
+  headerStyle: { backgroundColor: "#2C6BED" },
+  headerTitleStyle: { color: "#26272c" },
+  headerTintColor: "#26272c",
+};
+
+// #26272c
 export default function MainContainer() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator screenOption={globalScreenOptions}>
         <Tab.Screen name="Friends" component={FriendsScreen} />
         <Tab.Screen name="Profile" component={ProfileScreen} />
       </Tab.Navigator>
