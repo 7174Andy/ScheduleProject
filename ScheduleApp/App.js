434f237aae048212768bc82364615d74e51fafdc
7174Andy <hep003@ucsd.edu>
2024-04-18 13:58:44 -0700

    added temporary Navigation bar

--- ScheduleApp/App.js
+++ ScheduleApp/App.js
@@ -1,10 +1,52 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
 import ProfileScreen from "./app/screens/ProfileScreen";
-import MainContainer from "./app/navigator/MainContainer";
+import FriendsScreen from "./app/screens/FriendsScreen";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return <MainContainer />;
+  return (
+    <>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <Tab.Navigator screenOptions={{ headerShown: false }}>
+          <Tab.Screen
+            name="Friends"
+            component={FriendsScreen}
+            options={{
+              tabBarLabel: "Friends",
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons
+                  name="account-multiple"
+                  color={color}
+                  size={size}
+                />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Profile"
+            component={ProfileScreen}
+            options={{
+              tabBarLabel: "Profile",
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons
+                  name="account"
+                  color={color}
+                  size={size}
+                />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({});
