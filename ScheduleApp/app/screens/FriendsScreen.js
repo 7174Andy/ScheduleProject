bfe557b3e1ffba39bf2f46f6235c7eb0bb71bc81
jinseo215 <jic089@ucsd.edu>
2024-05-02 19:08:50 -0700

    friends screen

--- ScheduleApp/app/screens/FriendsScreen.js
+++ ScheduleApp/app/screens/FriendsScreen.js
@@ -11,20 +11,33 @@ import {
 } from "react-native";
 
 import colors from "../config/colors";
+import IconButton from "../components/ui/IconButton";
 
 export default function FriendsScreen() {
   return (
     <SafeAreaView style={styles.background}>
-      <Text style={styles.title}>Friends Screen</Text>
+      {/* <Text style={styles.title}>Friends Screen</Text> */}
+      <View style={styles.inputContainer}>
       <TextInput style={styles.hashtagInput} 
       placeholder='#hashtag'
       placeholderTextColor= 'white'/>
-      <View style={styles.container}>
-        <Text style={styles.containerText}>friends</Text>
+      <Pressable style={styles.searchButton}>
+      <IconButton
+                icon="search"
+                color='white'
+                size={20}/>
+      </Pressable>
       </View>
+    <View style={styles.containerContainer}>
       <View style={styles.container}>
-        <Text style={styles.containerText}>suggestions</Text>
+        <Text style={styles.containerTitle}>Friends</Text>
+        <View style={styles.containerLine}/>
+        </View>
+      <View style={styles.container}>
+        <Text style={styles.containerTitle}>Requests</Text>
+        <View style={styles.containerLine}/>
       </View>
+    </View>
     </SafeAreaView>
   );
 }
@@ -37,26 +50,69 @@ const styles = StyleSheet.create({
   title: {
     color: 'white',
     fontSize: 32,
-    fontWeight: 'bold',
+    fontWeight: '700',
     margin: 16,
+    paddingBottom:0,
+  },
+  inputContainer:{
+    flexDirection: 'row',
   },
   hashtagInput: {
-    margin: 10,
+    marginLeft: 10,
+    marginBottom: 10,
+    marginTop: 20,
     padding: 8,
     borderWidth: 1,
     borderColor: 'white',
     borderRadius: 25,
+    color: 'white',
+    flex: 7,
+  },
+  containerContainer:{
+    flexDirection: 'column',
+    width: '100%',
+    height: '85%',
   },
   container:{
     margin: 10,
     backgroundColor: 'white',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '9',
-    height: '35%',
-    borderRadius: 25,
+    width: '5',
+    height: '40%',
+    paddingTop: 10,
+    paddingHorizontal: 5,
+    borderRadius: 15,
+    flex: 1,
   },
-  containerText:{
+  containerTitle:{
     color: 'black',
-  }
+    fontSize: 16,
+    fontWeight: '700',
+    marginTop: 2,
+    marginLeft: 12,
+  },
+  containerLine:{
+    marginHorizontal: 10,
+    marginVertical: 5,
+    height: 1,
+    backgroundColor: 'grey',
+    width: '92%',
+  },
+  friendContainer:{
+    marginHorizontal: 10,
+    marginVertical: 5,
+  },
+  friendName:{
+    color: 'black',
+    fontSize: 14,
+    fontWeight: '500',
+    marginLeft: 12,
+  },
+  searchButton:{
+    marginBottom: 10,
+    marginRight: 12,
+    marginLeft: 2,
+    marginTop: 20,
+    padding: 0,
+    flex: 1,
+  },
 });
