168f16c04a3586b54c00e7e314f1848ba726a31e
jinseo215 <jic089@ucsd.edu>
2024-05-15 01:33:48 -0700

    edit profile

--- ScheduleApp/app/screens/EditProfileScreen.js
+++ ScheduleApp/app/screens/EditProfileScreen.js
@@ -10,9 +10,30 @@ import {
     TextInput,
   } from "react-native";
   import colors from "../config/colors";
+  import IconButton from "../components/ui/IconButton";
+  import { useState } from "react";
 
 
 function EditProfileScreen() {
+
+
+    const [newHashtag, setnewHashtag] = useState(false);
+
+    const addHashtag = () => {
+        setnewHashtag(true);
+    }
+
+    const [hashtags, setHashtags] = useState(["#hashtag1", "#hashtag2", "#hashtag3"]); 
+
+    const [temporaryHashtag, setTemporaryHashtag] = useState("");
+
+    const addNewHashtag = () => {
+        setHashtags([...hashtags, temporaryHashtag]);
+        console.log(temporaryHashtag);
+        setTemporaryHashtag("");
+        setnewHashtag(false);
+    }
+
 
     return (
         <SafeAreaView style={styles.background}>
@@ -44,10 +65,30 @@ function EditProfileScreen() {
                         style={styles.inputText}
                         placeholder="Enter Password"
                         placeholderTextColor="grey"/>
-                        <TextInput
-                        style={styles.inputText}
-                        placeholder="Enter Hashtag"
-                        placeholderTextColor="grey"/>
+                        <View style={styles.hashtagContainer}>
+                            {hashtags.map((hashtag) => (
+                                <View style={styles.oneHashTag}>
+                                    <Text style={styles.hashtagText}>{hashtag}</Text>
+                                    <IconButton icon="close" color="white" size={20} onPress={()=>{
+                                        setHashtags(hashtags.filter((item) => item !== hashtag));
+                                    }}/>
+                                </View>
+                            ))}
+                            {!newHashtag
+                            ?
+                            <IconButton icon="add" color="white" size={20} onPress={()=>{addHashtag(true)}}/>
+                            :
+                            <View style={styles.row}>
+                                <TextInput
+                                    value={temporaryHashtag}
+                                    style={styles.hashtagText}
+                                    placeholder="Enter Hashtag"
+                                    placeholderTextColor="grey"
+                                    onChangeText={setTemporaryHashtag}/>
+                                <IconButton icon="add" color="white" size={20} onPress={addNewHashtag}/>
+                            </View>
+                            }
+                        </View>
                 </View>
             </View>
         </SafeAreaView>
@@ -75,8 +116,37 @@ const styles = StyleSheet.create({
     },
     inputText: {
       color: 'white',
-      fontSize: 18,
+      fontSize: 16,
       margin: 20,
-      
     },
+    hashtagContainer: {
+        flex: 0,
+        flexDirection: "column",
+        justifyContent: "center",
+        marginTop: 20,
+        marginLeft: 10,
+    },
+    hashtagText: {
+        color: colors.textColor,
+        fontSize: 15,
+    },
+    oneHashTag: {
+        backgroundColor: colors.hashtag,
+        borderRadius: 10,
+        padding: 5,
+        margin: 5,
+        marginRight: 10,
+        shadowColor: colors.black,
+        shadowOffset: {
+          width: 0,
+          height: 2,
+        },
+        justifyContent: "center",
+        flexDirection: "row",
+    },
+    hashtagText: {
+        color: 'white',
+        fontSize: 16,
+        margin: 5,
+      },
 });
